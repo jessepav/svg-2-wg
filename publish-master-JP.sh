624e8c5fbf20fa2d6e895074204645d4388b3eca
Jesse Pavel <jpavel@alum.mit.edu>
2023-07-10 14:00:06 -0400

    Create linked index.html

--- publish-master-JP.sh
+++ publish-master-JP.sh
@@ -2,5 +2,8 @@
 
 SVG_BUILD_NO_SINGLE_PAGE=1 python3 tools/build-py3.py 
 
+cd build/publish
+ln -sf Overview.html index.html
+
 echo -e "\n----------------------------"
 echo "HTML output saved in build/publish"
